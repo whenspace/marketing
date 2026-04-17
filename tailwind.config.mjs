e58fa6b4036d6683ed637f2c04734c0f9297648f
Marcin Jan Syngajewski <Archetylator@users.noreply.github.com>
2026-04-17 17:02:19 +0000

    Add astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      letterSpacing: {
+        tightest: '-0.04em',
+      }
+    },
+  },
+  plugins: [],
+}
